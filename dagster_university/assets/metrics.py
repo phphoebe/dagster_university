64b8c56b6ad90b6e84f112f3fcf67069f11d9e18
Phoebe Chen <phoebehch@gmail.com>
2024-05-29 20:32:49 -0400

    Add partitions

--- dagster_university/assets/metrics.py
+++ dagster_university/assets/metrics.py
@@ -1,7 +1,9 @@
-import requests
+from dagster import asset, AssetExecutionContext
+
 from dagster_duckdb import DuckDBResource
 from . import constants
-from dagster import asset
+
+from ..partitions import weekly_partition
 
 import plotly.express as px
 import plotly.io as pio
@@ -67,47 +69,50 @@ def manhattan_map() -> None:
 # assets/metrics.py
 
 @asset(
-    deps=["taxi_trips"]
+    deps=["taxi_trips"],
+    partitions_def=weekly_partition
 )
-def trips_by_week(database: DuckDBResource) -> None:
+def trips_by_week(context: AssetExecutionContext, database: DuckDBResource) -> None:
+    """
+      The number of trips per week, aggregated by week.
+    """
 
-    current_date = datetime.strptime("2023-01-01", constants.DATE_FORMAT)
-    end_date = datetime.now()
+    period_to_fetch = context.partition_key
 
-    result = pd.DataFrame()
+    # get all trips for the week
+    query = f"""
+        select vendor_id, total_amount, trip_distance, passenger_count
+        from trips
+        where pickup_datetime >= '{period_to_fetch}'
+            and pickup_datetime < '{period_to_fetch}'::date + interval '1 week'
+    """
 
-    while current_date < end_date:
-        current_date_str = current_date.strftime(constants.DATE_FORMAT)
-        query = f"""
-          select
-            vendor_id, total_amount, trip_distance, passenger_count
-          from trips
-          where pickup_datetime >= '{current_date_str}' and pickup_datetime < '{current_date_str}'::date + interval '1 week'
-        """
+    with database.get_connection() as conn:
+        data_for_month = conn.execute(query).fetch_df()
 
-        with database.get_connection() as conn:
-            data_for_week = conn.execute(query).fetch_df()
-
-        aggregate = data_for_week.agg({
-            "vendor_id": "count",
-            "total_amount": "sum",
-            "trip_distance": "sum",
-            "passenger_count": "sum"
-        }).rename({"vendor_id": "num_trips"}).to_frame().T  # type: ignore
-
-        aggregate["period"] = current_date
-
-        result = pd.concat([result, aggregate])
-
-        current_date += timedelta(days=7)
+    aggregate = data_for_month.agg({
+        "vendor_id": "count",
+        "total_amount": "sum",
+        "trip_distance": "sum",
+        "passenger_count": "sum"
+    }).rename({"vendor_id": "num_trips"}).to_frame().T  # type: ignore
 
     # clean up the formatting of the dataframe
-    result['num_trips'] = result['num_trips'].astype(int)
-    result['passenger_count'] = result['passenger_count'].astype(int)
-    result['total_amount'] = result['total_amount'].round(2).astype(float)
-    result['trip_distance'] = result['trip_distance'].round(2).astype(float)
-    result = result[["period", "num_trips", "total_amount",
-                     "trip_distance", "passenger_count"]]
-    result = result.sort_values(by="period")
+    aggregate["period"] = period_to_fetch
+    aggregate['num_trips'] = aggregate['num_trips'].astype(int)
+    aggregate['passenger_count'] = aggregate['passenger_count'].astype(int)
+    aggregate['total_amount'] = aggregate['total_amount'].round(
+        2).astype(float)
+    aggregate['trip_distance'] = aggregate['trip_distance'].round(
+        2).astype(float)
+    aggregate = aggregate[["period", "num_trips",
+                           "total_amount", "trip_distance", "passenger_count"]]
 
-    result.to_csv(constants.TRIPS_BY_WEEK_FILE_PATH, index=False)
+    try:
+        # If the file already exists, append to it, but replace the existing month's data
+        existing = pd.read_csv(constants.TRIPS_BY_WEEK_FILE_PATH)
+        existing = existing[existing["period"] != period_to_fetch]
+        existing = pd.concat([existing, aggregate]).sort_values(by="period")
+        existing.to_csv(constants.TRIPS_BY_WEEK_FILE_PATH, index=False)
+    except FileNotFoundError:
+        aggregate.to_csv(constants.TRIPS_BY_WEEK_FILE_PATH, index=False)
--- dagster_university/jobs/__init__.py
+++ dagster_university/jobs/__init__.py
@@ -1,13 +1,17 @@
 from dagster import define_asset_job, AssetSelection
+from ..partitions import monthly_partition, weekly_partition
+
 
 trips_by_week = AssetSelection.assets(["trips_by_week"])
 
 trip_update_job = define_asset_job(
     name="trip_update_job",
-    selection=AssetSelection.all() - trips_by_week
+    partitions_def=monthly_partition,  # partitions added here
+    selection=AssetSelection.all() - AssetSelection.assets(["trips_by_week"])
 )
 
 weekly_update_job = define_asset_job(
     name="weekly_update_job",
+    partitions_def=weekly_partition,
     selection=trips_by_week,
 )
